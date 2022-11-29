cd23bc23ae1dfaa8cd3227302ab3321d6e43cbb7
Virda09 <vrdajO9@gmail.com>
2022-11-29 15:40:28 +0700

    push baru

--- app/Http/Controllers/Admin/MitraController.php
+++ app/Http/Controllers/Admin/MitraController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class MitraController extends Controller
+class MitraController implements ControllerInterface
 {
     /**
      * Display a listing of the resource.
--- app/Models/Mitra.php
+++ app/Models/Mitra.php
@@ -13,7 +13,7 @@ class Mitra extends Model
 
     protected $fillable = [
         'name',
-        'namaAdmin',
+        'namaAdminPt',
         'jk',
         'notelp',
         'email',
